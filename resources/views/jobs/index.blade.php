e48fda728c452081801f0a1483bdd257633740ea
ezu0 <burimofumofu@gmail.com>
2024-12-13 07:55:44 +0900

    update: javasctiptからAlpine.js

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,20 +1,17 @@
 <x-layout>
   <x-breadcrumbs class="mb-4" :links="['求人一覧' => route('jobs.index')]" />
-  <x-card class="mb-4 text-sm">
-    <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
+  <x-card class="mb-4 text-sm" x-data="">
+    <form x-ref="filters" id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">検索</div>
-          <x-text-input name="search" value="{{ request('search') }}" placeholder="文字を入力してください"
-            form-id="filtering-form" />
+          <x-text-input name="search" value="{{ request('search') }}" placeholder="文字を入力してください" form-ref="filters" />
         </div>
         <div>
           <div class="mb-1 font-semibold">給与</div>
           <div class="flex space-x-2">
-            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="下限"
-              form-id="filtering-form" />
-            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="上限"
-              form-id="filtering-form" />
+            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="下限" form-ref="filters" />
+            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="上限" form-ref="filters" />
           </div>
         </div>
       </div>
@@ -28,7 +25,7 @@
           <x-radio-group name="category" :options="App\Models\Job::$category" />
         </div>
       </div>
-      <x-button>
+      <x-button class="w-full">
         <i class="fas fa-search mr-2"></i>絞り込み
       </x-button>
 
