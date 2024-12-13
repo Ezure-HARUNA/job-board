8f9447311ac169cb783da9630ab164a147b62fe0
ezu0 <burimofumofu@gmail.com>
2024-12-13 18:20:17 +0900

    feat: ログイン/ログアウト

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,9 +7,37 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <title>求人サイト</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
-<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-cyan-500 to-blue-500  text-slate-700">
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-700">
+  <nav class="mb-8 flex justify-between text-lg font-medium">
+    <ul class="flex space-x-2">
+      <li>
+        <a href="{{ route('jobs.index') }}">Home</a>
+      </li>
+    </ul>
+    <ul class="flex space-x-2">
+      @auth
+        <li>
+          {{ auth()->user()->name ?? 'ゲストユーザー' }}
+        </li>
+        <li>
+          <form x-data="{ open: false }"
+            @submit.prevent="open = confirm('本当にログアウトしますか？'); if (open) { $event.target.submit(); }"
+            action="{{ route('auth.destroy') }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">ログアウト</button>
+          </form>
+        </li>
+      @else
+        <li>
+          <a href="{{ route('auth.create') }}">ログイン</a>
+        </li>
+      @endauth
+    </ul>
+  </nav>
   {{ $slot }}
 </body>
 
